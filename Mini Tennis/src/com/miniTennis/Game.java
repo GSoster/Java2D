875096dd9fca5e41c7ce50a0b73831d22c592122
Guilherme Soster <guilhermesosters@gmail.com>
2014-07-06 21:52:05 -0300

    Adicionando classe Racquet e recebendo entrada do usuario

--- Mini Tennis/src/com/miniTennis/Game.java
+++ Mini Tennis/src/com/miniTennis/Game.java
@@ -3,15 +3,39 @@ package com.miniTennis;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Game extends JPanel {
 
 	Ball ball = new Ball(this);
+	Racquet racquet = new Racquet(this);
 
+	
+	public Game(){
+		addKeyListener(new KeyListener() {
+			
+			public void keyTyped(KeyEvent e){}
+			
+			public void keyReleased(KeyEvent e){
+				racquet.keyReleased(e);
+			}
+			
+			public void keyPressed(KeyEvent e){
+				racquet.keyPressed(e);
+			}
+			
+		});
+		setFocusable(true);
+		
+	}
+	
 	public void move() {
 		ball.move();
+		racquet.move();
 	}
 
 	@Override
@@ -21,6 +45,7 @@ public class Game extends JPanel {
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
 		ball.paint(g2d);
+		racquet.paint(g2d);
 	}
 
 	
---
+++ Mini Tennis/src/com/miniTennis/Racquet.java
@@ -0,0 +1,45 @@
+package com.miniTennis;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+public class Racquet{
+
+		int x = 0;
+		int xa = 0;
+		private Game game;
+		
+		public Racquet(Game game){
+			this.game = game;
+		}
+		
+		public void move(){
+			if(x + xa > 0 && x + xa < game.getWidth() - 60){
+				x = x + xa;
+			}
+		}
+		
+		public void paint(Graphics2D g){
+			g.fillRect(x, 330, 60, 10);
+		}
+		
+		public void keyReleased(KeyEvent e ){
+			xa = 0;
+		}
+		
+		/**
+		 * Verifica se a tecla precionada foi 'direita' ou 'esquerda'
+		 * e seta a direcao do movimento (xa) de acordo.
+		 * Cabe salientar que a classe Racquet so se move na horizontal
+		 * portanto nao temos propriedade y
+		 * @param e
+		 */
+		public void keyPressed(KeyEvent e){
+			if(e.getKeyCode() == KeyEvent.VK_LEFT){
+				xa = -1;
+			}
+			if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+				xa = 1;
+			}
+		}
+	
+}
